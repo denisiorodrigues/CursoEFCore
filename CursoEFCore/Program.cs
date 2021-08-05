aaf7197e0717a7a66765dc7be094f1c99e5f3efb
= <=>
2021-08-05 20:28:59 -0300

    Atualizando registro

--- CursoEFCore/Program.cs
+++ CursoEFCore/Program.cs
@@ -12,7 +12,8 @@ namespace App
         {
             //Console.WriteLine("Hello World!");
             //InserindoDados();
-            InserirDadosEmMassa();
+            //InserirDadosEmMassa();
+            TrabalhandoComChamado();
         }
 
         private static void InserirDadosEmMassa()
@@ -84,6 +85,43 @@ namespace App
 
             var registros = db.SaveChanges();
             Console.WriteLine($"Total Registros: {registros}");
+        }
+
+        private static void AtualizarDados()
+        {
+             using var db = new ApplicationContext();
+            //1-opção
+            //db.Produtos.Add(produto);
+            //2-opção
+            //db.Set<Produto>().Add(produto);
+            //3 - opção : Forçando o rastreamento
+            //db.Entry(produto).State = EntityState.Added;
+            //db.Add(produto);
+
+            var result = db.Chamados.Find(1);
+
+            var registros = db.SaveChanges();
+        }
+
+        private static void TrabalhandoComChamado()
+        {
+            var chamado =  new Chamado()
+            {
+                Codigo = "123",
+                Titulo = "NovoTitulo"              
+            };
+
+            using var db = new ApplicationContext();
+
+            //db.Entry(chamado).State = EntityState.Added;
+            //db.Entry(chamado).State = EntityState.Ed;
+            var chamado2 = db.Chamados.Find("123");
+
+            chamado2.Titulo = "Novo titulo 2";
+
+            //db.Attach(chamado2);
+            db.Update(chamado2);         
+            db.SaveChanges();
         }
     }
 }
